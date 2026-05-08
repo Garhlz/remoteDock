7e4ab5df4afeb8402f3b5d3c2636f1ed633a8482
elaine <garhlz257@gmail.com>
2026-05-08 15:16:35 +0800

    Initial Commit

---
+++ remoteDock/remoteDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  remoteDockApp.swift
+//  remoteDock
+//
+//  Created by Elaine on 2026/5/8.
+//
+
+import SwiftUI
+
+@main
+struct remoteDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
